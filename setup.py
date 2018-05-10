a24a56e9f9aac417f73a84420b7edd89742ed84a
Jan Palounek <palounek@me.com>
2018-05-10 12:16:37 +0200

    Basic pip structure.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='unida',
+	version='0.1',
+	description='Unida Client',
+	url='https://github.com/unida/client-python.git',
+	author='Unida',
+	author_email='info@unida.io',
+	license='MIT',
+	packages=['unida'],
+	zip_safe=False)
